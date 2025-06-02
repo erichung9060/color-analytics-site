bb13b3ae9979bf738a427e443c3d90e4c2161ef2
erichung9060 <rthung96@gmail.com>
2025-06-02 19:45:20 +0000

    Test: add CICD workflows

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   allowedDevOrigins: ['10.2.0.115'],
-
+  output: 'export',
 };
 
 export default nextConfig;
